e479f7557c601bb51b4c77c99a77c94a4f26fc5c
Ashley Littlewood <ashley.littlewood22@gmail.com>
2023-04-05 08:31:41 +0100

    GetAllItemsByName & GetItemsByName to tree class

--- LLCodingAssessment/Tree.cs
+++ LLCodingAssessment/Tree.cs
@@ -11,6 +11,31 @@ namespace LLCodingAssessment
     public class Tree
     {
         // Root Node (Folder) of the tree
-        public Folder Root { get; set; } 
+        public Folder Root { get; set; }
+
+        public List<Item> GetAllItemsByName(string itemName)
+        {
+            return GetItemsByName(Root, itemName);
+        }
+
+        private List<Item> GetItemsByName(Folder folder, string itemName)
+        {
+            List<Item> items = new List<Item>();
+
+            foreach (var node in folder.Nodes)
+            {
+                if (node is Item item && item.Name == itemName)
+                {
+                    items.Add(item);
+                }
+
+                if (node is Folder subFolder)
+                {
+                    items.AddRange(GetItemsByName(subFolder, itemName));
+                }
+            }
+
+            return items;
+        }
     }
 }
